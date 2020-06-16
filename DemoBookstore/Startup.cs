dc8fe86c7076387b25c71ef07715c79a03f7d014
Adeola <40776751+Adexandria@users.noreply.github.com>
2020-06-16 19:18:01 +0100

    Update Startup.cs

--- DemoBookstore/Startup.cs
+++ DemoBookstore/Startup.cs
@@ -38,11 +38,11 @@ namespace DemoBookstore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+           if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DemoBookstore")))
             {
-                app.UseDeveloperExceptionPage();
+                Console.WriteLine("Use https redirection");
+                app.UseHttpsRedirection();
             }
-            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
